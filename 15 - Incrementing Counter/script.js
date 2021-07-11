42e197dd6dac3ab5880a820f58b979da60be89ba
khusanov-m <khusanov.it.tech@gmail.com>
2021-07-11 16:14:45 +0500

    script

---
+++ 15 - Incrementing Counter/script.js
@@ -0,0 +1,20 @@
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target"),
+      c = +counter.innerText,
+      increment = target / 500;
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCounter();
+});
