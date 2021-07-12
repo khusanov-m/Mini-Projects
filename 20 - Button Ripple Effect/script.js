5f0041e98e37b850797f5bbbfb6cb3a96fbc9db1
khusanov-m <khusanov.it.tech@gmail.com>
2021-07-12 15:43:31 +0500

    script

---
+++ 20 - Button Ripple Effect/script.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll(".ripple");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    const x = e.clientX,
+      y = e.clientY,
+      buttonTop = e.target.offsetTop,
+      buttonLeft = e.target.offsetLeft,
+      xInside = x - buttonLeft,
+      yInside = y - buttonTop;
+
+    const circle = document.createElement("span");
+    circle.classList.add("circle");
+    circle.style.top = yInside + "px";
+    circle.style.left = xInside + "px";
+
+    this.appendChild(circle);
+
+    setTimeout(() => circle.remove(), 500);
+  });
+});
