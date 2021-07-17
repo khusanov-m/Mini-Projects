dc6654a6b6ee5ae1b8a740dc1988f16041ed821b
khusanov-m <khusanov.it.tech@gmail.com>
2021-07-17 19:54:01 +0500

    script

---
+++ 30 - Auto Text Effect/script.js
@@ -0,0 +1,22 @@
+const textEl = document.getElementById("text"),
+  speedEl = document.getElementById("speed"),
+  text = "I Love Programming!";
+
+let idx = 1,
+  speed = 300 / speedEl.value;
+
+writeText();
+
+function writeText() {
+  textEl.innerText = text.slice(0, idx);
+
+  idx++;
+
+  if (idx > text.length) {
+    idx = 1;
+  }
+
+  setTimeout(writeText, speed);
+}
+
+speedEl.addEventListener("input", (e) => (speed = 300 / e.target.value));
